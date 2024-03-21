833a0a6c42d700435c2678a6c206e8af9487a6e0
chrysanthemum <asingiser@gmail.com>
2024-03-21 03:09:52 -0400

    fix import

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-const {formSubmission, myFetch, pickPlanet, addDestinationInfo} = require('./scriptHelper.js');
+// const {formSubmission, myFetch, pickPlanet, addDestinationInfo} = require('./scriptHelper.js');
 
 window.addEventListener("load", function() {
-
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse = myFetch();
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-        let planet = pickPlanet(listedPlanets);
-        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
-    })
-
     let form = document.querySelector("form");
     
     form.addEventListener("submit", (event) => {
@@ -28,5 +14,17 @@ window.addEventListener("load", function() {
 
         formSubmission(document, list, pilotNameInput, copilotNameInput, fuelLevelInput, cargoMassInput);
     })
-    
+
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let planet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
+    })
  });
